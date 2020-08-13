f0e0db694baa2c4783b19ed46f9c901e76c673eb
Aakash Appalaneni <aakash.appalaneni@outlook.com>
2020-08-13 18:49:43 -0500

    testing changes

--- 2.H.py
+++ 2.H.py
@@ -1,3 +1,15 @@
+# (x, y, z) = [1, 1, 2]
+# n = 3
+#
+# for i in range(x + 1):
+#     for j in range(y + 1):
+#         for k in range(z + 1):
+#             if (i + j + k) != n:
+#                 print([i, j, k])
+# var = [[a, b, c] for a in range(x + 1) for b in range(y + 1) for c in range(z + 1) if (a + b + c) != n]
+# print(var)
+#
+#
 # def bubbleSort(myList):
 #     lengthOfList = len(myList)
 #
