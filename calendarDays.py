00cc93af9aa25fa000b202d3e8f2b4af5b8f6478
Aakash Appalaneni <aakash.appalaneni@outlook.com>
2020-09-29 18:21:18 -0500

    my new modifications

--- calendarDays.py
+++ calendarDays.py
@@ -11,4 +11,3 @@ import calendar
 dt = input().split(" ")
 wk = calendar.weekday(int(dt[2]), int(dt[0]), int(dt[1]))
 print(calendar.day_name[wk].upper())
-
--- doormatProblem.py
+++ doormatProblem.py
@@ -1,29 +1,1 @@
-# https://www.hackerrank.com/challenges/designer-door-mat/problem
-#
-# Sample Designs
-#
-#     Size: 7 x 21
-#     ---------.|.---------
-#     ------.|..|..|.------
-#     ---.|..|..|..|..|.---
-#     -------WELCOME-------
-#     ---.|..|..|..|..|.---
-#     ------.|..|..|.------
-#     ---------.|.---------
-
-di = list(map(int, input().split()))
-rows = di[0]
-columns = di[1]
-# print the top half
-for i in range(int(rows / 2)):
-    printString = ".|." * ((i * 2) + 1)
-    print(printString.center(columns, "-"))
-
-# print the middle line
-mid = 'WELCOME'.center(columns, '-')
-print(mid)
-
-# print the bottom half
-for i in reversed(range(int(rows / 2))):
-    printString = ".|." * ((i * 2) + 1)
-    print(printString.center(columns, "-"))
+# https://www.hackerrank.com/challenges/designer-door-mat/problem## Sample Designs##     Size: 7 x 21#     ---------.|.---------#     ------.|..|..|.------#     ---.|..|..|..|..|.---#     -------WELCOME-------#     ---.|..|..|..|..|.---#     ------.|..|..|.------#     ---------.|.---------di = list(map(int, input().split()))rows = di[0]columns = di[1]# print the top halffor i in range(rows // 2):    printString = ".|." * ((i * 2) + 1)    print(printString.center(columns, "-"))# print the middle linemid = 'WELCOME'.center(columns, '-')print(mid)# print the bottom halffor i in reversed(range(rows // 2)):    printString = ".|." * ((i * 2) + 1)    print(printString.center(columns, "-"))
