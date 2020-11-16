ed361d963b37dc5112220c7b9897d9a7323cc1d5
Aakash Appalaneni <aakash.appalaneni@outlook.com>
2020-11-16 16:17:45 -0600

    my banana homework

---
+++ minionGame.py
@@ -0,0 +1,22 @@
+def minion_game(s):
+    vowels = ["A", "E", "I", "O", "U"]
+
+    kevsc = 0
+    stusc = 0
+    for i in range(len(s)):
+        if s[i] in vowels:
+            kevsc += (len(s) - i)
+        else:\\
+            stusc += (len(s) - i)
+
+    if kevsc > stusc:
+        print("Kevin", kevsc)
+    elif kevsc < stusc:
+        print("Stuart", stusc)
+    else:
+        print("Draw")
+
+
+if __name__ == '__main__':
+    s = input()
+    minion_game(s)
