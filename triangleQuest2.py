a2955f019bb277aa8d724c1857b70dd95051af62
Aakash Appalaneni <aakash.appalaneni@outlook.com>
2020-11-16 19:33:46 -0600

    my added links to some work

--- triangleQuest2.py
+++ triangleQuest2.py
@@ -1,2 +1,16 @@
+# https://www.hackerrank.com/challenges/triangle-quest-2/problem
+#Print the palindromic triangle of size  as explained above.
+#
+# Sample Input
+#
+# 5
+# Sample Output
+#
+# 1
+# 121
+# 12321
+# 1234321
+# 123454321
+
 for i in range(1, int(input()) + 1):
     print((10 ** i // 9) ** 2)
