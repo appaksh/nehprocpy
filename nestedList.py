d5cf322cbfa4bb215589663be4663c2c8cd4354c
Aakash Appalaneni <aakash.appalaneni@outlook.com>
2020-11-19 19:43:25 -0600

    my new adds to nested list

--- nestedList.py
+++ nestedList.py
@@ -1,29 +1,1 @@
-# https://www.hackerrank.com/challenges/nested-list/problem?h_r=internal-search
-# Print the name(s) of any student(s) having the second lowest grade in. If there are multiple students, order their names alphabetically and print each one on a new line.
-#
-# Sample Input 0
-#
-# 5
-# Harry
-# 37.21
-# Berry
-# 37.21
-# Tina
-# 37.2
-# Akriti
-# 41
-# Harsh
-# 39
-# Sample Output 0
-#
-# Berry
-# Harry
-
-myList = []
-
-for n in range(int(input())):
-    myList.append([input(), float(input())])
-
-secondLowest = sorted(list(set(score for (name, score) in myList)))[1]
-
-print("\n".join(sorted(list(name for (name, score) in myList if score == secondLowest))))
+# https://www.hackerrank.com/challenges/nested-list/problem?h_r=internal-search# Print the name(s) of any student(s) having the second lowest grade in. If there are multiple students, order their names alphabetically and print each one on a new line.## Sample Input 0## 5# Harry# 37.21# Berry# 37.21# Tina# 37.2# Akriti# 41# Harsh# 39# Sample Output 0## Berry# HarrymyList = []for n in range(int(input())):    myList.append([input(), float(input())])secondLowest = sorted(list(set(score for (name, score) in myList)))[1]print("\n".join(sorted(list(name for (name, score) in myList if score == secondLowest))))
