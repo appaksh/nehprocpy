0ffde4ccdc155004c24536136ffdc32cb0f27d88
Aakash Appalaneni <aakash.appalaneni@outlook.com>
2020-11-29 18:09:15 -0600

    my home workp

---
+++ permutations.py
@@ -0,0 +1,1 @@
+# https://www.hackerrank.com/challenges/itertools-permutations/problem# Output Format## Print the permutations of the string  on separate lines.## Sample Input## HACK 2# Sample Output## AC# AH# AK# CA# CH# CK# HA# HC# HK# KA# KC# KHfrom itertools import permutationss,k = input().split()for x in permutations(sorted(s),int(k)):    print("".join(x))
