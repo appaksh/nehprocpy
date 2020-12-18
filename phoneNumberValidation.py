15ba3bf4eaf865c503d0b1fd8be58abf687dbcaf
Aakash Appalaneni <aakash.appalaneni@outlook.com>
2020-12-17 19:50:30 -0600

    my fun regex validation HW

---
+++ phoneNumberValidation.py
@@ -0,0 +1,1 @@
+import redef isValidPhoneNumber(phoneNumber):    if re.match(r"[7-9]\d{9}$", phoneNumber):        return "YES"    else:        return "NO"print("\n".join([isValidPhoneNumber(str(input())) for myNumbers in range(int(input()))]))
