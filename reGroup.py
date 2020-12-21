c371b6dc7f9765e082e1e9d040d171b462d4c7bc
Aakash Appalaneni <aakash.appalaneni@outlook.com>
2020-12-21 14:33:27 -0600

    my new regex reGroup.py upupdate

--- reGroup.py
+++ reGroup.py
@@ -1,1 +1,1 @@
-import remySyntax = re.search(r"([a-zA-Z0-9$])\1+", input().strip())print(mySyntax.group(1) if mySyntax else -1)
+import remySyntax = re.search(r"([a-zA-Z0-9$])\1", input().strip())print(mySyntax.group(1) if mySyntax else -1)
