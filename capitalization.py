cb757754026b4e7f3260d25a99827e99261f239d
Aakash Appalaneni <aakash.appalaneni@outlook.com>
2020-12-28 20:28:04 -0600

    my newest december hw

---
+++ capitalization.py
@@ -0,0 +1,1 @@
+#!/bin/python3import os# Complete the solve function below.def solve(s):    s = s.split(" ")    return " ".join(myCapNum.capitalize() for myCapNum in s)if __name__ == '__main__':    fptr = open(os.environ['OUTPUT_PATH'], 'w')    s = input()    result = solve(s)    fptr.write(result + '\n')    fptr.close()
