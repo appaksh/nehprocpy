ae5ab7b35e70291173c3b53bd20daf86d0f113a8
Aakash Appalaneni <aakash.appalaneni@outlook.com>
2021-01-01 12:21:03 -0600

    fixing the line breaks

--- HTMLParser.py
+++ HTMLParser.py
@@ -1,1 +1,21 @@
-from abc import ABCfrom html.parser import HTMLParserclass MyHTMLParser(HTMLParser, ABC):    def handle_starttag(self, tag, attrs):        print("Start :", tag)        [print(f"-> {attr[0]} > {attr[1]}") for attr in attrs]    def handle_endtag(self, tag):        print("End   :", tag)    def handle_startendtag(self, tag, attrs):        print("Empty :", tag)        [print(f"-> {attr[0]} > {attr[1]}") for attr in attrs]parser = MyHTMLParser()for _ in range(int(input())):    parser.feed(input())
+from abc import ABC
+from html.parser import HTMLParser
+
+
+class MyHTMLParser(HTMLParser, ABC):
+
+    def handle_starttag(self, tag, attrs):
+        print("Start :", tag)
+        [print(f"-> {attr[0]} > {attr[1]}") for attr in attrs]
+
+    def handle_endtag(self, tag):
+        print("End   :", tag)
+
+    def handle_startendtag(self, tag, attrs):
+        print("Empty :", tag)
+        [print(f"-> {attr[0]} > {attr[1]}") for attr in attrs]
+
+
+parser = MyHTMLParser()
+for _ in range(int(input())):
+    parser.feed(input())
--- reStartAndEnd.py
+++ reStartAndEnd.py
@@ -1,1 +1,11 @@
-import res = input()k = input()pattern = re.compile(k)match = pattern.search(s)if not match:    print("(-1, -1)")while match:    print(f"({match.start()}, {match.end() - 1})")    match = pattern.search(s, match.start() + 1)
+import re
+
+s = input()
+k = input()
+pattern = re.compile(k)
+match = pattern.search(s)
+if not match:
+    print("(-1, -1)")
+while match:
+    print(f"({match.start()}, {match.end() - 1})")
+    match = pattern.search(s, match.start() + 1)
