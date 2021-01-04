f3a4d18e2798d1ea0b97d1454c1619ebfaa8c3ea
Aakash Appalaneni <aakash.appalaneni@outlook.com>
2021-01-04 12:20:11 -0600

    my final regex problem regex subsitution...

---
+++ regexSubsitution.py
@@ -0,0 +1,1 @@
+for _ in range(int(input())):    myReplacements = input()    while ' && ' in myReplacements or ' || ' in myReplacements:        myReplacements = myReplacements.replace(" && ", " and ").replace(" || ", " or ")    print(myReplacements)
