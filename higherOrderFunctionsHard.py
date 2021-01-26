b8811bfba60bd18ed0b22e68ac8b9275684b7db4
Aakash Appalaneni <aakash.appalaneni@outlook.com>
2021-01-25 19:07:38 -0600

    my functions other example

--- higherOrderFunctionsHard.py
+++ higherOrderFunctionsHard.py
@@ -1,32 +1,22 @@
-# Input will have list of Students
-# Each Student will have name, grade, sport
-# You should
-#  print students in the following format
-#   student <name in Capitals> with grade <grade in upper case> plays <sport in capitals>
-#  before printing you should be able apply one of these filters
-#  1. grade filter, which filters for students with Grade "A"
-#  2. sport filter, which filters for students with sport "running"
-
-# Return list of grade _ students
-def gradeFilter(allStudents, filterValue):
-    return list([student for student in allStudents if student[1].upper() == filterValue.upper()])
-
-# Return list of sport _ students
-def sportFilter(allStudents, filterValue):
-    return list([student for student in allStudents if student[2].upper() == filterValue.upper()])
+def carFilter(allCars, filterValue):
+    return list([car for car in allCars if car[0].upper() == filterValue.upper()])
 
 
-def printStudentsInfo(students, filterFunc, filterValue):
-    filteredStudents = filterFunc(students, filterValue)
-    for student in filteredStudents:
-        print(f'student {student[0].capitalize()} with grade {student[1].upper()} plays {student[2].capitalize()}')
+def speedFilter(allCars, filterValue):
+    return list([car for car in allCars if car[2].upper() == filterValue.upper()])
 
 
-myStudents = [
-    ["aakash", "a", "basketball"],
-    ["kaushik", "b", "tennis"],
-    ["siri", "c", "volleyball"],
+def printCarInfo(cars, filterFunc, filterValue):
+    filteredCars = filterFunc(cars, filterValue)
+    print(*[f"Car {car[0].capitalize()} {car[1].upper()} is {car[2].capitalize()}\n" for car in filteredCars])
+
+
+myCars = [
+    ["hennessy", "F5", "fast"],
+    ["bugatti", "super sport", "fast"],
+    ["bugatti", "chiron", "Fast"],
 ]
 
-printStudentsInfo(myStudents, gradeFilter, "b")
-printStudentsInfo(myStudents, sportFilter, "volleyball")
+printCarInfo(myCars, carFilter, "hennessy")
+print("--------------------------------------------------------------------------------------------------------------")
+printCarInfo(myCars, speedFilter, "fast")
