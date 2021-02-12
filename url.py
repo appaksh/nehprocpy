0f6cbec7357845549f30ac7afd88b0086806796d
Aakash Appalaneni <aakash.appalaneni@outlook.com>
2021-02-11 19:32:26 -0600

    mostly up of url hw

--- url.py
+++ url.py
@@ -31,18 +31,18 @@ def parseUrl(url):
 
 
 def printUrl(pattern):
-    print("Scheme :", getScheme(pattern))
-    print("Domain :", getDomain(pattern))
-    print("Port :", getPort(pattern))
-    print("Path :", getPath(pattern))
+    print("Scheme : ", getScheme(pattern))
+    print("Domain : ", getDomain(pattern))
+    print("Port : ", getPort(pattern))
+    print("Path : ", getPath(pattern))
     print("")
 
 
 def compareUrls(url1, url2):
     pattern1 = parseUrl(url1)
     pattern2 = parseUrl(url2)
-    url1Parts = [getScheme(pattern1), getDomain(pattern1) , getPort(pattern1), getPath(pattern1)]
-    url2Parts = [getScheme(pattern2), getDomain(pattern2) , getPort(pattern2), getPath(pattern2)]
+    url1Parts = [getScheme(pattern1), getDomain(pattern1), getPort(pattern1), getPath(pattern1)]
+    url2Parts = [getScheme(pattern2), getDomain(pattern2), getPort(pattern2), getPath(pattern2)]
 
     if url1Parts == url2Parts:
         return True
@@ -51,4 +51,4 @@ def compareUrls(url1, url2):
 
 
 urls = input("Enter urls: ").split(";")
-print("urls equal? ", compareUrls(urls[0].strip(), urls[1].strip()))
+print("urls equal : ", compareUrls(urls[0].strip(), urls[1].strip()))
