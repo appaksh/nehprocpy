64943483eedcc4771fe30a6971a32e276b134618
Aakash Appalaneni <aakash.appalaneni@outlook.com>
2021-02-27 18:09:01 -0600

    my time stamp with weather data apis

--- decoratorTimeStamp.py
+++ decoratorTimeStamp.py
@@ -1,28 +1,34 @@
+import json
+
 import time
 import urllib.request
 
 
 def trackingOverallTime(functionToBeDecorated):
-    print("Time:")
-
-    def timeTracker():
-        print("Start")
+    def timeTracker(*args):
         startTime = int(time.time() * 1000)
-        functionToBeDecorated()
+        data = functionToBeDecorated(*args)
         endTime = int(time.time() * 1000)
-        print(
-            f"Tracking function: {functionToBeDecorated.__name__}, overall time: {endTime - startTime} milliseconds")
+        print(f"Tracking function: {functionToBeDecorated.__name__}, overall time: {endTime - startTime} milliseconds ")
+        return data
 
     return timeTracker
 
 
 @trackingOverallTime
-def printTimeFuncDecorated():
+def getWeatherData():
     url = "https://api.weather.gov/gridpoints/TOP/31,80/forecast/hourly"
-    file = urllib.request.urlopen(url)
-    print("Result code: " + str(file.getcode()))
+    handle = urllib.request.urlopen(url)
+    data = handle.read().decode()
+    return data
 
 
-time.sleep(1)
+@trackingOverallTime
+def parseWeatherData(data):
+    jsonWeather = json.loads(data)
+    periods = jsonWeather["properties"]["periods"]
+    [print(f'{period["startTime"][11:16]} {period["temperature"]}{period["temperatureUnit"]} ') for period in periods]
 
-printTimeFuncDecorated()
+
+weather = getWeatherData()
+parseWeatherData(weather)
