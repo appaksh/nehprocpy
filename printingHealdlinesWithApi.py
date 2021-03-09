fd1cf91a0084f40dfaeeda8f7c81ef042c4974cd
Aakash Appalaneni <aakash.appalaneni@outlook.com>
2021-03-08 20:27:49 -0600

    getting headline fo rthe weather api uped version

--- printingHealdlinesWithApi.py
+++ printingHealdlinesWithApi.py
@@ -1,18 +1,28 @@
 import json
 import urllib.request
 
+def weatherWrapper(*args):
+    if weatherDict.get(args[0]):
+        print("Reading from local cache ")
+    else:
+        alerts = getWeatherData(*args)
+        weatherDict[args[0]] =  alerts
 
-def getWeatherData():
-    url = f'https://api.weather.gov/alerts/active?area={str(input()).upper()} '
+    return weatherDict[args[0]]
+
+
+def getWeatherData(stateName):
+    print("Reading from url ")
+    url = f'https://api.weather.gov/alerts/active?area={stateName}'
     handle = urllib.request.urlopen(url)
     data = handle.read().decode()
-    return data
-
-
-def parseWeatherData(data):
     jsonWeather = json.loads(data)
     features = jsonWeather["features"]
     return [feature["properties"]["headline"] for feature in features]
 
 
-print("\n".join(parseWeatherData(getWeatherData())))
+weatherDict = {}
+for _ in range(4):
+    state = str(input()).upper()
+    alerts = weatherWrapper(state)
+    print(f'Total alerts for {state} = {len(alerts)} ')
