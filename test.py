6a4ef3660ffdd6acbbe5beabd3229b478cac1f9b
Aakash Appalaneni <aakash.appalaneni@outlook.com>
2021-06-14 13:35:24 -0500

    inheritance and f format

---
+++ test.py
@@ -0,0 +1,1 @@
+name = input("What is your name: ")school = input("What school do you got to: ")grade = int(input("What grade are you in: "))print(f"{name} does to {school} and he is in grade #{grade} ")
