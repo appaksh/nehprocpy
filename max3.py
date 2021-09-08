c5e4d67af385aa53ebfdfccfca1d3a5456ab3b20
Aakash Appalaneni <aakash.appalaneni@gmail.com>
2021-09-07 19:22:28 -0500

    lil changes

--- max3.py
+++ max3.py
@@ -1,6 +1,7 @@
-numList = [12, 52, 33, 21]
+numList = [12, 52, 33, 21, 56]
 maxNum = numList[0]
 for element in numList:
+    # maxNum = element if element > maxNum else maxNum
     if maxNum < element:
         maxNum = element
 
